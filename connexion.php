4a72ed77494e4a4ebebdd8312e7ebf032388dc99
ChapuisBenjamin <BenjaminChapuis27@gmail.com>
2026-06-02 08:52:48 +0200

    ajout connexion.php

---
+++ connexion.php
@@ -0,0 +1,22 @@
+<?php
+// 1. Configuration des identifiants de connexion (Spécial XAMPP)
+define('HOST', 'localhost');
+define('DB_NAME', 'exposition_mmi'); // Le nom exact de ta base phpMyAdmin
+define('USER', 'root');              // Identifiant par défaut sous XAMPP
+define('PASS', '');                  // TOUJOURS VIDE sous XAMPP Windows !
+
+try {
+    // 2. Tentative de connexion avec PDO et configuration UTF-8
+    $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME . ";charset=utf8", USER, PASS);
+    
+    // 3. Activation des alertes en cas d'erreur SQL
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Message de test (on le supprimera dès que ça marchera)
+    // echo "Connexion réussie à la base de données de l'exposition !";
+
+} catch (PDOException $e) {
+    // Si un truc plante, XAMPP nous dira exactement quoi via ce message
+    echo "Erreur de connexion : " . $e->getMessage();
+    die();
+}
